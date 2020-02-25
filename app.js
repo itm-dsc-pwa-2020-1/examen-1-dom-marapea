7fc819a829f4f5141fd018615e574a78b38efafa
marapea <marapea0197@gmail.com>
2020-02-25 13:59:20 -0600

    parte 2

--- app.js
+++ app.js
@@ -3,20 +3,30 @@ function WHITE(){
   dark.classList.remove("seccion1");
   dark.classList.add("seccion12");
   console.log(dark);
-  alert("Se guardaron los datos personales");
+
   
 
   var dark2=document.querySelector("section");
   dark2.classList.remove("seccion2");
   dark2.classList.add("seccion22");
   console.log(dark2);
+
+  var dark2=document.querySelector("div");
+  dark2.classList.remove("texto");
+  dark2.classList.add("texto1");
+  console.log(dark2);
 }
 
 function BLACK(){
   var white=document.querySelector("header");
-  white.classList.remove("seccion1");
-  white.classList.add("seccion12");
+  white.classList.remove("seccion12");
+  white.classList.add("seccion1");
   console.log(white);
-  alert("Se guardaron los datos personales");
+
+  var white2=document.querySelector("section");
+  white2.classList.remove("seccion22");
+  white2.classList.add("seccion2");
+  console.log(white2);
+  alert("white");
 
 }
